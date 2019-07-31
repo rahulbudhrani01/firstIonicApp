fefd889da9ed81baf880ed1af96e9b352b078cbf
Rahul Budhrani <rahulbudhrani01@gmail.com>
2019-08-01 02:07:05 +0530

    First App

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  height: number;
+  weight: number;
+  bmiValue: number;
+  bmiMessage: number;
+
 
   constructor() {}
+
+  calculateBMI() {
+    if (this.weight > 0 && this.height > 0) {
+      let finalBmi = this.weight / (this.height / 100 * this.height / 100);
+      this.bmiValue = parseFloat(finalBmi.toFixed(2));
+      this.setBMIMessage();
+    }
+  }
+  
+  // setBMIMessage will set the text message based on the value of BMI
+  private setBMIMessage() {
+    if (this.bmiValue < 18.5) {
+      this.bmiMessage = "Underweight"
+    }
+  
+    if (this.bmiValue > 18.5 && this.bmiValue < 25) {
+      this.bmiMessage = "Normal"
+    }
+  
+    if (this.bmiValue > 25 && this.bmiValue < 30) {
+      this.bmiMessage = "Overweight"
+    }
+  
+    if (this.bmiValue > 30) {
+      this.bmiMessage = "Obese"
+    }
+  }
 
 }
